1ab455b10a708a88744f5b5739f56aaf6047ef0e
lun-weichang <lunweichang539@gmail.com>
2019-11-09 15:10:52 -0500

    Added clear cookie and remove session id function when user logout

--- public/js/form.js
+++ public/js/form.js
@@ -2,13 +2,6 @@
     const loginForm = document.getElementById("loginForm");
     const userEmail = document.getElementById("userEmail");
     const userPassword = document.getElementById("userPass");
-    // const noEmail = document.getElementById("noEmail");
-    // const noPassword = document.getElementById("noPassword");
-    // const invalidEmail = document.getElementById("invalidEmailFormat");
-
-    // noEmail.style.display = 'none';
-    // noPassword.style.display = 'none';
-    // invalidEmail.style.display = 'none';
     $("#noEmail").hide();
     $("#noPassword").hide();
     $("#invalidEmailFormat").hide();
@@ -21,7 +14,10 @@
             //Could email be non-string type?
             if (userEmail.value && !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail.value))) {
                 $("#invalidEmailFormat").show();
+                $("#noEmail").hide();
                 event.preventDefault();
+            } else {
+                $("#invalidEmailFormat").hide();
             }
 
             if (!userEmail.value || userEmail.value.length == 0) {
@@ -32,12 +28,6 @@
                 $("#noPassword").show();
                 event.preventDefault();
             }
-            //is there a better way to display the errors?
-            //use an array to keep all the errors
-            //create p tag for each error and append them to a list?
-            // $("#noEmail").hide();
-            // $("#noPassword").hide();
-            // $("#invalidEmailFormat").hide();
         });
     }
 })();
