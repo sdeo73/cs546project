3138c01387cf1b74032ebc7eb8ae5091ea7ca0c1
lun-weichang <lunweichang539@gmail.com>
2019-11-09 09:29:28 -0500

    Added insertSessionID and getHashPassword helper functions

--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,20 @@ const router = express.Router();
 const data = require('../data');
 const loginData = data.login;
 const errorMessages = require('../public/errorMessages');
+const passwordHash = require('password-hash');
+const bcrypt = require("bcrypt");
 
 router.get('/', async (req, res) => {
     try {
-        // res.render('mainPage', {tab_title: "Main Page"});
-        res.render('posts/loginPage', {tab_title: "Login Page"});
+
+        if (req.session.AuthCookie) { //AuthCookie exists
+            //gets the user info from user collection with sessionID
+            
+            //redirect to home page?
+
+        } else {
+            res.render('posts/loginPage', {tab_title: "Login Page"});
+        }
         return res.status(200);
     } catch (error) {
         return res.status(404).json(error);
@@ -16,25 +25,47 @@ router.get('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        console.log("Enter login routes");
+        let errors = [];
         const user_email = req.body.user_email;
         const user_pass = req.body.user_password;
         console.log(`email = ${user_email}`);
         console.log(`password = ${user_pass}`);
-        // const user_email = req.params.email;
-        // const user_pass = req.params.pass;
-        const loginInfo = await loginData.loginValidation(user_email, user_pass);
-        
-        if(loginInfo == true){
-            res.render('posts/loginSuccess', {tab_title: "Successfully Login", user_email: user_email, user_pass: user_pass});
-            return res.status(200).json({loginSuccess: "Login successfully"});
-        }else{
-            return res.status(400).json({error: loginInfo});
+        const currentUser = await loginData.getHashPassword(user_email);
+
+        let validPass = false;
+        if (currentUser !== null) { //user exists
+            const hashPass = currentUser.password;
+            console.log(`hashPass = ${hashPass}`);
+            validPass = passwordHash.verify(user_pass, hashPass);
         }
-        
-        // return res.status(200);
+        if (validPass) {
+            //sets the AuthCookie value to session id
+            req.session.AuthCookie = req.sessionID;
+            req.session.user_id = currentUser._id;
+            //inserts the session id to user collection
+            loginData.insertSessionID(currentUser._id, req.sessionID);
+
+            //redirect to home page
+            res.render('posts/loginSuccess', {tab_title: "Successfully Login", user_email: user_email, user_pass: user_pass});
+            return res.status(200);
+        } else {
+            //redirect to the login page again
+            res.redirect('/');
+            return res.status(401).json({error: validPass});
+        }
     } catch (error) {
         return res.status(404).json(error);
+    }
+});
+
+router.get('/logout', async (req, res) => {
+    try {
+        //Expire the cookies and render the logoutPage
+        req.session.destroy(res.render('pages/logoutPage', {tab_title: "Logout Successfully Page"}));
+        //removes the session id from the user collection
+
+    } catch (err) {
+        return res.status(404).json(err);
     }
 });
 
