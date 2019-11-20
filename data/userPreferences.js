107b8634c3807782ed1656fb1e70b4d5137839ad
Shiwani Deo <sdeo@stevens.edu>
2019-11-20 13:04:33 -0500

    Add functions for updating user's preferences

--- data/userPreferences.js
+++ data/userPreferences.js
@@ -2,29 +2,51 @@ const ObjectId = require('mongodb').ObjectID;
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
 const error = require('../public/errorMessages');
+const userFunctions = require('users');
 
+/**
+ * Function to check if user with userID has already selected userPreferences
+ * @param {*} userID 
+ * @returns Boolean value stating is user preference object exists in user collection, else returns errors array
+ */
 async function checkUserPreferenceExists(userID) {
     let errors = [];
-    if(!userID) {
+    if (!userID) {
         errors.push(error.noUserIDWasProvided);
-    } if(!ObjectId.isValid(userID)) {
+    } if (!ObjectId.isValid(userID)) {
         errors.push(error.userIDInvalid);
-    } if(errors.length>0) {
+    } if (errors.length > 0) {
         return errors;
     }
     const usersCollection = await users();
-    const user = await usersCollection.findOne({_id: new ObjectId(userID)});
-    if(user==null) {
-        throw new Error(error.userDoesNotExist); 
+    const user = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (user == null) {
+        throw new Error(error.userDoesNotExist);
     }
-    if('userPreferences' in user) {
+    if ('userPreferences' in user) {
         return true;
     } else {
         return false;
     }
 }
 
-async function addUserPreferences(gender, dob, mealPreference, tourType, nTravelers, specialNeeds, budget, destination, travelDateStart, travelDateEnd, userID) {
+/**
+     * Creates a new user preference object and inserts it into the users collection.
+     * @param {*} gender User's gender
+     * @param {*} dob User's birthday in yyyy-mm-dd
+     * @param {*} mealPreference An array of meal preferences
+     * @param {*} tourType String representing tour type
+     * @param {*} tourActivity String representing tour activity
+     * @param {*} nTravelers Integer representing number of travelers
+     * @param {*} specialNeeds An array of special needs
+     * @param {*} budget Integer representing budget per person, minimum limit 2000
+     * @param {*} destination String representing destination (city)
+     * @param {*} travelDateStart User's travel start date in yyyy-mm-dd
+     * @param {*} travelDateEnd User's travel end date in yyyy-mm-dd
+     * @param {*} userID User's ID retreived from session
+     * @returns true if insertion was successful, else returns errors array
+     */
+async function addUserPreferences(gender, dob, mealPreference, tourType, tourActivity, nTravelers, specialNeeds, budget, destination, travelDateStart, travelDateEnd, userID) {
     let errors = [];
     if (!gender) {
         errors.push(error.genderMissing);
@@ -34,9 +56,11 @@ async function addUserPreferences(gender, dob, mealPreference, tourType, nTravel
         errors.push(error.mealPrefMissing)
     } if (!tourType) {
         errors.push(error.tourTypeMissing);
+    } if (!tourActivity) {
+        errors.push(error.tourActivityMissing);
     } if (!nTravelers) {
         errors.push(error.nTravelersMissing);
-    } if(!specialNeeds) {
+    } if (!specialNeeds) {
         errors.push(error.specialNeedsMissing);
     } if (!budget) {
         errors.push(error.budgetMissing);
@@ -47,21 +71,27 @@ async function addUserPreferences(gender, dob, mealPreference, tourType, nTravel
     } if (errors.length > 0) {
         return errors;
     }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+
     const usersCollection = await users();
 
     let userPreferences = {
         mealPreference: {
-        vegan: mealPreference.includes("vegan"),
-		vegetarian: mealPreference.includes("vegetarian"),
-		whiteMeat: mealPreference.includes("whiteMeat"),
-		redMeat: mealPreference.includes("redMeat"),
-		seafood: mealPreference.includes("seafood"),
-		eggs: mealPreference.includes("eggs")
+            vegan: mealPreference.includes("vegan"),
+            vegetarian: mealPreference.includes("vegetarian"),
+            whiteMeat: mealPreference.includes("whiteMeat"),
+            redMeat: mealPreference.includes("redMeat"),
+            seafood: mealPreference.includes("seafood"),
+            eggs: mealPreference.includes("eggs")
         },
         tourType: tourType,
-        noOfTravelers: nTravelers,
+        tourActivity: tourActivity,
+        noOfTravelers: parseInt(noOfTravelers),
         specialNeeds: specialNeeds,
-        budget: budget,
+        budget: parseInt(budget),
         destination: destination,
         travelDates: {
             start: travelDateStart,
@@ -69,7 +99,7 @@ async function addUserPreferences(gender, dob, mealPreference, tourType, nTravel
         }
     };
 
-    const userObject = await usersCollection.updateOne({_id: new ObjectId(userID)}, {$set: {gender: gender, dob: dob, userPreferences: userPreferences}});
+    const userObject = await usersCollection.updateOne({ _id: new ObjectId(userID) }, { $set: { gender: gender, dob: dob, userPreferences: userPreferences } });
     if (userObject.updatedCount === 0) {
         throw new Error(error.userPrefCreationError);
     } else {
@@ -77,7 +107,276 @@ async function addUserPreferences(gender, dob, mealPreference, tourType, nTravel
     }
 }
 
+/**
+ * Function to update user's meal preference.
+ * @param {*} userID User ID to update
+ * @param {*} newMealPreference An array of meal preferences
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateUserMealPref(userID, newMealPreference) {
+    let errors = [];
+    if (!newMealPreference) {
+        errors.push(error.mealPrefMissing);
+    } else if (!Array.isArray(newMealPreference)) {
+        throw new Error(error.mealPrefInvalidType);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.mealPreference': newMealPreference } });
+    if (!updatedObject) {
+        throw new Error(error.mealPrefUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update user's tour activity.
+ * @param {*} userID User ID to update
+ * @param {*} newTourActivity  String representing tour activity
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateUserTourActivity(userID, newTourActivity) {
+    let errors = [];
+    if (!newTourActivity) {
+        errors.push(error.tourActivityMissing);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.tourActivity': newTourActivity } });
+    if (!updatedObject) {
+        throw new Error(error.tourActivityUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update user's tour type.
+ * @param {*} userID User ID to update
+ * @param {*} newTourType  String representing tour type
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateUserTourType(userID, newTourType) {
+    let errors = [];
+    if (!newTourType) {
+        errors.push(error.tourTypeMissing);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.tourType': newTourType } });
+    if (!updatedObject) {
+        throw new Error(error.tourTypeUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update number of travelers.
+ * @param {*} userID User ID to update
+ * @param {*} newNTravelers  Integer representing number of travelers
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateNumOfTravelers(userID, newNTravelers) {
+    let errors = [];
+    if (!newNTravelers) {
+        errors.push(error.nTravelersMissing);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.nTravelers': parseInt(newNTravelers) } });
+    if (!updatedObject) {
+        throw new Error(error.nTravelersUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update user's special needs
+ * @param {*} userID User ID to update
+ * @param {*} newSpecialNeeds  An array of special needs
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateSpecialNeeds(userID, newSpecialNeeds) {
+    let errors = [];
+    if (!newSpecialNeeds) {
+        errors.push(error.specialNeedsMissing);
+    } else if (!Array.isArray(newSpecialNeeds)) {
+        throw new Error(error.specialNeedsInvalidType);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.specialNeeds': newSpecialNeeds } });
+    if (!updatedObject) {
+        throw new Error(error.specialNeedsUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update user's budget
+ * @param {*} userID User ID to update
+ * @param {*} newBudget Integer representing budget per person, minimum limit 2000
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateBudget(userID, newBudget) {
+    let errors = [];
+    if (!newBudget) {
+        errors.push(error.budgetMissing);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.budget': parseInt(budget) } });
+    if (!updatedObject) {
+        throw new Error(error.budgetUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update user's destination
+ * @param {*} userID User ID to update
+ * @param {*} newDestination String representing destination (city)
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateUserDestination(userID, newDestination) {
+    let errors = [];
+    if (!newDestination) {
+        errors.push(error.destinationMissing);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    const updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.destination': newDestination } });
+    if (!updatedObject) {
+        throw new Error(error.destinationUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+/**
+ * Function to update user's travel dates, user can update either one date, or both
+ * @param {*} userID User ID to update
+ * @param {*} newTravelStartDate User's travel start date in yyyy-mm-dd
+ * @param {*} newTravelEndDate User's end start date in yyyy-mm-dd
+ * @returns Updated user object if successful, else throws error.
+ */
+async function updateTravelDates(userID, newTravelStartDate, newTravelEndDate) {
+    let errors = [];
+    if (!newTravelStartDate && !newTravelEndDate) {
+        errors.push(error.travelDatesMissing);
+    }
+
+    if (errors.length > 0) {
+        return errors;
+    }
+
+    if (!userID) {
+        throw new Error(error.userIDMissing);
+    }
+    const usersCollection = await users();
+    const userToUpdate = await usersCollection.findOne({ _id: new ObjectId(userID) });
+    if (!userToUpdate) {
+        throw new Error(error.userDoesNotExist);
+    }
+    let updatedObject;
+    if (!newTravelStartDate) {
+        updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.travelDates.end': newTravelEndDate } });
+    } else if (!newTravelEndDate) {
+        updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.travelDates.start': newTravelStartDate } });
+    } else {
+        updatedObject = await usersCollection.updateOne(userToUpdate, { $set: { 'userPreferences.travelDates.start': newTravelStartDate, 'userPreferences.travelDates.end': newTravelEndDate } });
+    }
+    if (!updatedObject || updatedObject.modifiedCount == 0) {
+        throw new Error(error.travelDateUpdationFailed);
+    } else {
+        return userFunctions.getUserById(userID);
+    }
+}
+
+
 
 module.exports = {
-    addUserPreferences, checkUserPreferenceExists
+    addUserPreferences, checkUserPreferenceExists, updateUserMealPref, updateUserTourActivity, updateUserTourType, updateNumOfTravelers, updateSpecialNeeds, updateBudget, updateUserDestination, updateTravelDates
 }
