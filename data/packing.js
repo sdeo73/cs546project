4f0b0fed9d6545e2a9c6d2dd59e71750da27008c
sushrutm29 <smadhavi@stevens.edu>
2019-11-19 19:50:38 -0500

    packing-functions v2

--- data/packing.js
+++ data/packing.js
@@ -1,10 +1,11 @@
 const ObjectId = require('mongodb').ObjectID;
 const mongoCollections = require('../config/mongoCollections');
 const packing = mongoCollections.packing;
+const errorMessages = require('./public/errorMessages');
 
 async function createPackingList(type, items){
-    if(!type || !items) throw new Error("Argument Missing!");
-    if(typeof type !== 'string' || !Array.isArray(items)) throw new Error("Argument not of correct type!");
+    if(!type || !items) throw new Error(errorMessages.packingListArgumentMissing);
+    if(typeof type !== 'string' || !Array.isArray(items)) throw new Error(errorMessages.packingListArgumentTypeError);
 
     const packingCollection = await packing();
 
@@ -15,91 +16,91 @@ async function createPackingList(type, items){
 
     const listExists = await packingCollection.findOne({"type" : type});
 
-    if(listExists) throw new Error("Packing list already exists!");
+    if(listExists) throw new Error(errorMessages.packingListAlreadyExists);
 
     const insertInfo = await packingCollection.insertOne(newList);
 
-    if(insertInfo == null) throw new Error('Insertion failed!');
+    if(insertInfo == null) throw new Error(errorMessages.packingListAddError);
 
-    if (insertInfo.insertedCount === 0) throw new Error('Could not add packing list!');
+    if (insertInfo.insertedCount === 0) throw new Error(errorMessages.packingListAddError);
 
     return true;
 
 }
 
 async function getPackingList(type){
-    if(!type) throw new Error("Type not entered!");
-    if(typeof type !== 'string') throw new Error("Type is not of proper type!");
+    if(!type) throw new Error(errorMessages.packingListArgumentMissing);
+    if(typeof type !== 'string') throw new Error(errorMessages.packingListArgumentTypeError);
 
     const packingCollection = await packing();
 
     const packingList = await packingCollection.findOne({"type": type});
 
-    if(!packingList) throw new Error("No packing list with that type!");
+    if(!packingList) throw new Error(errorMessages.packingListNotFound);
 
     return packingList.items;
 }
 
 async function addItemsToPackingList(type, items){
-    if(!type || !items) throw new Error("Argument Missing!");
-    if(typeof type !== 'string') throw new Error("Argument not of correct type!");
+    if(!type || !items) throw new Error(errorMessages.packingListArgumentMissing);
+    if(typeof type !== 'string') throw new Error(errorMessages.packingListArgumentTypeError);
 
     if(typeof items == 'string'){
         const packingCollection = await packing();
         const updationInfo = await packingCollection.updateOne({"type": type}, {$addToSet: {items: items}});
 
-        if(!updationInfo) throw new Error("Update Failed!");
-        if(updationInfo.updatedCount == 0) throw new Error("No packing list exists with that type!");
+        if(!updationInfo) throw new Error(errorMessages.packingListItemUpdateError);
+        if(updationInfo.updatedCount == 0) throw new Error(errorMessages.packingListItemUpdateError);
 
         return true;
     }else if(Array.isArray(items)){
         const packingCollection = await packing();
         const updationInfo = await packingCollection.updateOne({"type": type}, {$addToSet: {items: {$each: items}}});
 
-        if(!updationInfo) throw new Error("Update Failed!");
-        if(updationInfo.updatedCount == 0) throw new Error("No packing list exists with that type!");
+        if(!updationInfo) throw new Error(errorMessages.packingListItemUpdateError);
+        if(updationInfo.updatedCount == 0) throw new Error(errorMessages.packingListItemUpdateError);
 
         return true;
     }else{
-        throw new Error("Argument not of correct type!");
+        throw new Error(errorMessages.packingListArgumentTypeError);
     }
 }
 
 async function deletePackingList(type){
-    if(!type) throw new Error("Type not entered!");
-    if(typeof type !== 'string') throw new Error("Type is not of proper type!");
+    if(!type) throw new Error(errorMessages.packingListArgumentMissing);
+    if(typeof type !== 'string') throw new Error(errorMessages.packingListArgumentTypeError);
 
     const packingCollection = await packing();
     const deletionInfo = await packingCollection.removeOne({"type": type});
 
-    if(!deletionInfo) throw new Error("Delete Failed!");
-    if(deletionInfo.deletedCount ==0) throw new Error("No packing list exists with that type!");
+    if(!deletionInfo) throw new Error(errorMessages.packingListDeleteFailed);
+    if(deletionInfo.deletedCount ==0) throw new Error(errorMessages.packingListDeleteFailed);
 
     return true;
 }
 
 async function removeItemsFromPackingList(type, items){
-    if(!type || !items) throw new Error("Argument Missing!");
-    if(typeof type !== 'string') throw new Error("Argument not of correct type!");
+    if(!type || !items) throw new Error(errorMessages.packingListArgumentMissing);
+    if(typeof type !== 'string') throw new Error(errorMessages.packingListArgumentTypeError);
 
     if(typeof items == 'string'){
         const packingCollection = await packing();
         const updationInfo = await packingCollection.updateOne({"type": type}, {$pull: {items: items}});
 
-        if(!updationInfo) throw new Error("Removal Failed!");
-        if(updationInfo.updatedCount == 0) throw new Error("No packing list exists with that type!");
+        if(!updationInfo) throw new Error(errorMessages.packingListItemRemovalFailed);
+        if(updationInfo.updatedCount == 0) throw new Error(errorMessages.packingListItemRemovalFailed);
 
         return true;
     }else if(Array.isArray(items)){
         const packingCollection = await packing();
-        const updationInfo = await packingCollection.updateOne({"type": type}, {$pull: {items: {$each: items}}});
+        const updationInfo = await packingCollection.updateOne({"type": type}, {$pull: {items: {$in: items}}});
 
-        if(!updationInfo) throw new Error("Removal Failed!");
-        if(updationInfo.updatedCount == 0) throw new Error("No packing list exists with that type!");
+        if(!updationInfo) throw new Error(errorMessages.packingListItemRemovalFailed);
+        if(updationInfo.updatedCount == 0) throw new Error(errorMessages.packingListItemRemovalFailed);
 
         return true;
     }else{
-        throw new Error("Argument not of correct type!");
+        throw new Error(errorMessages.packingListArgumentTypeError);
     }
 }
 
