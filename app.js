7b4bb0e986c8714c1d36134b0198df9954342a36
snaik9 <52750846+snaik9@users.noreply.github.com>
2019-11-26 10:11:17 -0800

    Signup fixed , app.js issue solved

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+const static = express.static(__dirname + '/public');
+const session = require('express-session');
+
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
+
+app.use('/public', static);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  name: 'AuthCookie',
+  secret: 'Patrick is awesome',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { path: '/', httpOnly: true, secure: false, maxAge: null }
+}));
+
+app.use(function (req, res, next) {
+  let auth = "Non-Authenticated User";
+  if (req.session.AuthCookie) {
+    auth = "Authenticated User";
+  }
+  console.log(`[${new Date().toUTCString()}]: ${req.method} ${req.originalUrl} (${auth})`);
+  next();
+});
+
+app.use('/home', function (req, res, next) {
+  if (req.session.userID) {
+    return next();
+  } else {
+    return res.status(200).redirect('/login');
+  }
+});
+
+app.use('/signup', function (req, res, next) {
+  if (req.session.userID) {
+    return res.status(200).redirect('/home');
+  } else {
+    return next();
+  }
+});
+
+app.use('/preferences', function (req, res, next) {
+  if (req.session.userID) {
+    return next();
+  } else {
+    return res.status(200).redirect('/login');
+  }
+});
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+configRoutes(app);
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const signup = require('./signup');
 const loginRoutes = require('./login');
 const userPrefRoutes = require('./userPreferences');
 const forgotpassword = require('./forgotpassword');
+const home=require('./home');
 
 
 const constructorMethod = app => {
@@ -10,6 +11,7 @@ const constructorMethod = app => {
     app.use("/", signup);
     app.use("/", userPrefRoutes);
     app.use("/forgotpassword",forgotpassword);
+    app.use("/",home);
     app.use("*", (req, res) => {
       res.sendStatus(404);
     });
