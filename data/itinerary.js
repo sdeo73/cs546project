ef85ac90b81a7677a144e73135f81d40e48d0bea
lun-weichang <lunweichang539@gmail.com>
2019-11-27 15:36:13 -0500

    1) Changed luxury type to leisure, 2)Added sortByTourType function into itinerary.js

--- data/itinerary.js
+++ data/itinerary.js
@@ -23,20 +23,59 @@ function compare(a, b) {
     return 0;
 }
 
-/** */
-function sortThingsToDo(thingsToDo) {
-    let sortedThingsToDo = {};
-    for (let obj in thingsToDo) {
-        //key = tourType, value = an array of thingsToDo objects with the same tourType
-        let temp = [];
-        if (thingsToDo[obj].type in sortedThingsToDo) {   //tour type already exists
-            temp = sortedThingsToDo[thingsToDo[obj].type];
-        }
-        temp.push(thingsToDo[obj]);
-        sortedThingsToDo[thingsToDo[obj].type] = temp;
+/**
+ * Selects an array of things to do based on the given two constraints: maximum budget and time (hours per day).
+ * Throws errors if invalid arguments were given or empty array is generated.
+ * 
+ * @param allThings an array of objects with things to do
+ * @param maxBudget maximum total budget allotted
+ * @param maxTime maximum time allotted
+ * @returns 
+ */
+async function selectAllThings(allThings, maxBudget, maxTime, tourType) {
+    if (arguments.length != 4 || !allThings || !maxBudget || !maxTime || !tourType) {
+        throw new Error(errorMessages.itineraryArgumentMissing);
     }
-    console.log(JSON.stringify(sortedThingsToDo));
-    return sortedThingsToDo;
+    
+    if (typeof allThings != "object" || Number.isNaN(maxBudget) || Number.isNaN(maxTime) || typeof tourType != "string") {
+        throw new Error(errorMessages.itineraryArgumentIncorrectType);
+    }
+
+    let finalItems = [];
+    // for (let thing in allThings) {
+    //     let currentItem = allThings[thing];
+    //     console.log(`currentItem = ${currentItem}`);
+    // }
+    console.log(`allThings[tourType] = ${JSON.stringify(allThings[tourType])}`);
+    if (allThings[tourType].length > 0) {
+        console.log(`TourType ${tourType} exists`);
+    }
+    
+    return finalItems;
+}
+
+/** */
+function sortAllThings(allThings, tourTypeList) {
+    let sortedAllThings = {};
+    for (let obj in allThings) {
+        //key = tourType, value = an array of allThings objects with the same tourType
+        let temp = [];
+        let objTourType = allThings[obj].type.toLowerCase();
+        if (objTourType in sortedAllThings) {   //tour type already exists
+            temp = sortedAllThings[objTourType];
+        }
+        temp.push(allThings[obj]);
+        sortedAllThings[objTourType] = temp;
+        // sortedAllThings[tourTypeList[]]
+    }
+    let resultThings = [];
+    for (let i = 0; i < tourTypeList.length; i++) {
+        let currentItems = sortedAllThings[tourTypeList[i]];
+        resultThings = resultThings.concat(currentItems);//can't do this
+    }
+
+    // return sortedAllThings;
+    return resultThings;
 }
 
 /** */
@@ -51,28 +90,49 @@ async function generateCompleteItinerary(userPreferences) {
     }
     let destinationObj = await destinationFunctions.getDestinationById(userPreferences.destinationId);
 
-    let thingsToDo = destinationObj.thingsToDo;
+    let allThings = destinationObj.thingsToDo;
     let restaurants = destinationObj.restaurants;
 
-    thingsToDo.sort(compare);   //sorts the thingsToDo by tour type
+    //is this step necessary? Time efficiency wise?
+    allThings.sort(compare);   //sorts the allThings by tour type
+    let tourTypeList = generateTourTypePriority(userPreferences.tourType);
+    // console.log(JSON.stringify(allThings.sort(compare)));
+    let sortedThings = sortAllThings(allThings, tourTypeList);
 
-    // console.log(JSON.stringify(thingsToDo.sort(compare)));
-    sortThingsToDo(thingsToDo);
+    console.log(JSON.stringify(sortedThings));
+    
+    //allThings, maxBudget, maxTime
+    // await selectAllThings(sortedThings, userPreferences.maxBudgetPerPerson, userPreferences.hoursPerDay, userPreferences.tourType);
 }
 
+function generateTourTypePriority(tourType) {
+    //User can select: Business, Hiking, Scenic, Adventure, Historical, Sightseeing
+    //Destination type: Leisure, Shopping, Historical, Sightseeing, Theme Park, Adventure, Entertainment, Museum
+    let priorityList = {
+        business: ["shopping", "sightseeing", "leisure", "entertainment", "museum", "historical", "theme park", "adventure"],
+        hiking: ["adventure", "sightseeing", "historical", "museum", "shopping", "leisure", "entertainment", "theme park"],
+        scenic: ["leisure", "sightseeing", "shopping", "entertainment", "museum", "historical", "theme park", "adventure"],
+        adventure: ["adventure", "sightseeing", "theme park", "historical", "shopping", "entertainment", "museum", "leisure"],
+        historical: ["historical", "museum", "sightseeing", "shopping", "entertainment", "leisure", "theme park", "adventure"],
+        sightseeing: ["sightseeing", "historical", "museum", "shopping", "entertainment", "theme park", "leisure", "adventure"]
+    }
+    return priorityList[tourType.toLowerCase()];
+}
+
+/** */
 async function main() {
     try {
         //10, 1000, 14, 5
         //destinationId, tourType, timePerDay, maxBudgetPerPerson, noOfDays, noOfTravellers
         let userPreferences = {
-            destinationId: "5dde98e1de6ddc6150e5aa75",
+            destinationId: "5ddedd9410fe736d50e80b75",
             tourType: "hiking",
             hoursPerDay: 10,
             maxBudgetPerPerson: 1000,
             numOfDays: 14,
             numOfTravelers: 5
         };
-        let resultItinerary = generateCompleteItinerary(userPreferences);
+        let resultItinerary = await generateCompleteItinerary(userPreferences);
     } catch (err) {
         console.log(err.message);
     }
