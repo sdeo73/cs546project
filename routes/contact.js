933f04158fc97703a279feaceef9244c44ad8c9d
snaik9 <52750846+snaik9@users.noreply.github.com>
2019-12-11 11:49:51 -0800

    route

--- routes/contact.js
+++ routes/contact.js
@@ -1,12 +1,16 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/contact', async (req, res) => {
     try {
-        res.status(200).render("pages/contact", {
-            title: "Contact"
+           res.status(200).render("pages/contact", {
+            title: "Contact Us"
         });
-    } catch (error) {
+    } 
+    
+    catch (error) 
+    {
         console.log("not found");
         res.sendStatus(404);
     }
--- routes/error404.js
+++ routes/error404.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/error404', async (req, res) => {
-    try {
-        res.status(200).render("pages/error404", {
+    try 
+    {
+       res.status(200).render("pages/error404", {
             title: "error404"
         });
     } catch (error) {
