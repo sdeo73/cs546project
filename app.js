a7cf1c5af9f5be9d6b01351c8ab9e2e788769234
sushrutm29 <smadhavi@stevens.edu>
2019-12-13 17:02:18 -0500

    removed few unused modules and protected login route

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ app.use('/home', function (req, res, next) {
   }
 });
 
+app.use('/login', function (req, res, next) {
+  if (req.session.userID) {
+    return res.status(200).redirect('/home');
+  } else {
+    return next();
+  }
+});
+
 app.use('/signup', function (req, res, next) {
   if (req.session.userID) {
     return res.status(200).redirect('/home');
