b196c1621c40553f9e1b88d56212f3e673907fd2
Shiwani Deo <sdeo@stevens.edu>
2019-12-14 21:27:23 -0500

    Fix navbar name on main preference form

--- routes/userPreferences.js
+++ routes/userPreferences.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
+const usersFunctions = data.users;
 const userPrefData = data.userPreferences;
 const xss = require("xss");
 
@@ -9,67 +10,69 @@ router.get('/preferences', async (req, res) => {
         if (await data.userPreferences.checkUserPreferenceExists(req.session.userID)) {
             res.status(200).redirect('/viewItinerary');
         } else {
-            res.status(200).render('pages/userPreferencesForm', { title: "Tell Us What You Like", partial: "preferences-scripts" });
+            let user = await usersFunctions.getUserById(req.session.userID);
+            let userName = user.firstName + " " + user.lastName;
+            res.status(200).render('pages/userPreferencesForm', { title: "Tell Us What You Like", name: userName, partial: "preferences-scripts" });
         }
     } catch (e) {
-        res.status(404).render("pages/somethingWentWrong",  {title: "Something Went Wrong"});
+        res.status(404).render("pages/somethingWentWrong", { title: "Something Went Wrong" });
     }
 })
 
 router.post('/preferences', async (req, res) => {
     try {
         const gender = xss(req.body.gender, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const dob = xss(req.body.dob, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const mealPref = xss(req.body.mealPref, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const tourType = xss(req.body.tourType, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const tourActivity = xss(req.body.tourActivity, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const nTravelers = xss(req.body.nTravelers, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         let specialNeeds = xss(req.body.specialNeeds, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const budget = xss(req.body.budget, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const city = xss(req.body.city, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const travelDateStart = xss(req.body.travelDateStart, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
         const travelDateEnd = xss(req.body.travelDateEnd, {
-            whiteList: [], 
+            whiteList: [],
             stripIgnoreTag: true,
             stripIgnoreTagBody: []
         });
@@ -80,15 +83,15 @@ router.post('/preferences', async (req, res) => {
             specialNeeds = false;
         }
         const userPref = await userPrefData.addUserPreferences(gender, dob, mealPref, tourType, tourActivity,
-                nTravelers, specialNeeds, budget, city, travelDateStart, travelDateEnd, req.session.userID);
-        
+            nTravelers, specialNeeds, budget, city, travelDateStart, travelDateEnd, req.session.userID);
+
         if (!userPref) {
-            return res.status(400).json({ error: userPref });
+            res.status(400).render("pages/somethingWentWrong", { title: "Something Went Wrong" });
         } else {
             return res.status(200).redirect('/generateItinerary');
         }
     } catch (e) {
-        res.status(404).render("pages/somethingWentWrong",  {title: "Something Went Wrong"});
+        res.status(404).render("pages/somethingWentWrong", { title: "Something Went Wrong" });
     }
 })
 
